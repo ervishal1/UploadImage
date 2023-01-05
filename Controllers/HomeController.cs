620896ad7fa4ae8728a66c12a03625ffa422aa7d
Vishal Singal <vishalsingal6@gmal.com>
2023-01-05 16:57:58 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using UploadImage.Data;
+using UploadImage.Models;
+using UploadImage.ViewModel;
+
+namespace UploadImage.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment WebHostEnvironment;
+
+        public HomeController(ApplicationDbContext context,IWebHostEnvironment webHostEnvironment)
+        {
+            _context = context;
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            var items = _context.Students.ToList();
+            return View(items);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(StudentViewModel vm)
+        {
+            string stringFileName = UploadFile(vm);
+            var Student = new Student
+            {
+                Name = vm.Name,
+                ProfileImage = stringFileName
+            };
+            _context.Students.Add(Student);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        private string UploadFile(StudentViewModel vm)
+        {
+            string fileName = null;
+            if(vm.ProfileImage != null)
+            {
+                string uploadDir = Path.Combine(WebHostEnvironment.WebRootPath, "Images");
+                fileName = Guid.NewGuid().ToString() + "-" + vm.ProfileImage.FileName;
+                string filePath = Path.Combine(uploadDir, fileName);
+                using var fileStream = new FileStream(filePath, FileMode.Create);
+                vm.ProfileImage.CopyTo(fileStream);
+            }
+            return fileName;
+        }
+    }
+}
---
+++ ViewModel/StudentViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UploadImage.ViewModel
+{
+    public class StudentViewModel
+    {
+        public string Name { get; set; }
+        public IFormFile ProfileImage { get; set; }
+    }
+}
